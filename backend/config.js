5dd6760712486f623d440025e933b0dd1cc19c5b
Alex Randall <122236229+randallcoding@users.noreply.github.com>
2024-08-16 05:09:45 +0100

    added mongodb + mongoose

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,6 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+    'mongodb+srv://alexrandall2002:root@cluster0.fjaqt.mongodb.net/games-collection?retryWrites=true&w=majority&appName=Cluster0'
+
+    // Please create a free database for yourself.
