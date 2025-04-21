5c0528ec855f4c4d552dee610c84d44e45990a4a
Alex Randall <122236229+randallcoding@users.noreply.github.com>
2025-04-21 17:32:13 +0100

    fixed major security issue

--- backend/index.js
+++ backend/index.js
@@ -1,42 +1,26 @@
+import "dotenv/config";
 import express from "express";
-import { PORT, mongoDBURL } from "./config.js";
-import mongoose from "mongoose";
-import { Game } from './models/gameModel.js'
+import mongoose from 'mongoose';
 import gamesRoute from './routes/gamesRoute.js';
-import cors from 'cors';
-
 const app = express();
 
-// Middleware for parsing request body
 app.use(express.json());
 
-// Middleware for handling CORS policy
-
-// app.use(
-    // cors({
-        // origin: 'http://localhost:5500',
-        // methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        // allowedHeaders: ['Content-Type'],
-
-    // })
-// );
-
-
 app.get('/', (request, response) => {
-    console.log(request)
-    return response.status(234).send('TEST')
+ console.log(request)
+ return response.status(234).send('TEST')
 });
 
 app.use('/games', gamesRoute);
 
 mongoose
-    .connect(mongoDBURL)
-    .then(() => {
-        console.log('App connected to database');
-        app.listen(PORT, () => {
-            console.log(`App is listening to port: ${PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+ .connect(process.env['mongoDBURL'])
+ .then(() => {
+ console.log('App connected to database');
+ app.listen(process.env['PORT'], () => {
+ console.log(`App is listening to port: ${process.env.PORT}`);
+ });
+ })
+ .catch((error) => {
+ console.log(error);
+ });
